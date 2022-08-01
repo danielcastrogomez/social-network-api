764de4e2338977f79087d94dc87deb6ac7f4288d
danielcastrogomez <ing.castrogomez@outlook.com>
2022-08-01 18:18:55 -0500

    Initial commit from local repo

---
+++ src/main/java/org/casdan/socialnetworkbackend/SocialNetworkBackendApplication.java
@@ -0,0 +1,13 @@
+package org.casdan.socialnetworkbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialNetworkBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialNetworkBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/casdan/socialnetworkbackend/SocialNetworkBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.casdan.socialnetworkbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialNetworkBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
